3c4882dbe7d52721f742b40e1283b012626a7391
Amit Kumar <106420527+amitjait@users.noreply.github.com>
2023-02-11 02:00:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -17,14 +17,27 @@ fetch('https://randomuser.me/api/')
 
     h2.innerHTML = `${data.name.first}  ${data.name.last}`;
     user.appendChild(h2);
-
-    console.log(data);
-    // console.log(data.results[0].cell);
-    console.log(data.dob.age);
-    console.log(data.email);
-    console.log(data.cell);
-    console.log(data.picture);
 });
+function getAnother(){
+    fetch('https://randomuser.me/api/')
+    .then((response) => response.json())
+    .then((result)=>{
+    
+        data = result.results[0];
+        let user = document.getElementById('user');
+        user.innerHTML = "";
+    
+        let img = document.createElement('img');
+        img.src = data.picture.large;
+        user.appendChild(img);
+    
+        let h2 = document.createElement('h2');
+        
+    
+        h2.innerHTML = `${data.name.first}  ${data.name.last}`;
+        user.appendChild(h2);
+    });
+}
 
 function Age(){
     let info = document.getElementById('info');
@@ -44,6 +57,3 @@ function Phone(){
     info.innerHTML = `${data.cell}`;
 }
 
-function getAnother(){
-    window.location.reload(); 
-}
